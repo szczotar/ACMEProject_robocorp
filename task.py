e95951d86b024ddf098b36d230fddf1a1dd6ab13
szczotar <szczotm>
2021-12-11 23:44:44 +0100

    work item test1

--- task.py
+++ task.py
@@ -1,5 +1,12 @@
 """Template robot with Python."""
 
+from RPA.Robocorp.WorkItems import WorkItems
+
+def queueAcme(item_id):
+    queue = WorkItems()
+    queue.create_output_work_item()
+    queue.set_work_item_variable("Age","28")
+    queue.save_work_item()
 
 
 def minimal_task():
@@ -7,4 +14,5 @@ def minimal_task():
 
 
 if __name__ == "__main__":
+    queueAcme(1)
     minimal_task()
