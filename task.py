02af26dc00f1172211833f6d36fbc4040c0c89ab
szczotar <szczotm>
2021-12-12 00:20:06 +0100

    work item test2

--- task.py
+++ task.py
@@ -4,9 +4,13 @@ from RPA.Robocorp.WorkItems import WorkItems
 
 def queueAcme(item_id):
     queue = WorkItems()
+    queue.get_input_work_item()
     queue.create_output_work_item()
-    queue.set_work_item_variable("Age","28")
-    queue.save_work_item()
+
+    queue.set_work_item_variable("Age", "28")
+    print(queue.list_work_item_variables())
+    
+  
 
 
 def minimal_task():
