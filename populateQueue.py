5bd9c89ce126d67878345d1351920802d5759f00
szczotar <szczotm>
2021-12-16 21:57:58 +0100

    queue populate test

---
+++ populateQueue.py
@@ -0,0 +1,33 @@
+"""Template robot with Python."""
+
+from RPA.Robocorp.WorkItems import WorkItems
+listAge = ["30", "28", "15"]
+def queueAcme():
+    queue = WorkItems()
+    queue.get_input_work_item()
+    
+    for name in listAge:
+
+
+        # queue.get_input_work_item()
+        queue.create_output_work_item()
+
+        queue.set_work_item_variable("Age", name)
+        queue.set_work_item_variable("Name", "Artur")
+        print(queue.list_work_item_variables())
+    
+        queue.save_work_item()
+    
+def minimal_task():
+    print("Done.")
+    queue = WorkItems()
+
+    
+    for i in range(3):
+        queue.get_input_work_item()
+        payload = queue.get_work_item_payload()
+        print(payload)
+
+if __name__ == "__main__":
+    queueAcme()
+    # minimal_task()
